653465a7faa07e4604b898b7606611ba1bb6e769
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-26 15:11:09 +0100

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :hyp, :opp, :adj
+
+  def initialize(hyp, opp, adj)
+    @hyp = hyp
+    @opp = opp
+    @adj = adj
+  end
+
+  def kind
+    if @opp <= 0 || @adj <= 0 || @hyp <= 0
+        raise TriangleError
+    end
+
+    if @opp + @adj <= @hyp || @adj + @hyp <= @opp || @hyp + @opp <= @adj
+      raise TriangleError
+    end
+
+    # :equilateral #all three sides equal
+    if @hyp == @adj && @hyp == @opp && @opp == @adj
+      :equilateral
+    # :scalene #no equal sides
+    elsif @hyp != @adj && @hyp != @opp && @opp != @adj
+      :scalene
+    # :isosceles #two equal sides
+    else
+      :isosceles
+    end
+
+  end
+
+  class TriangleError < StandardError
+    # def message
+    #   "This is not a valid triangle!"
+    # end
+  end
+
 end
